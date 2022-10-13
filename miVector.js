ad0b975a0205a3c775bcec43e9cf0377aa7b74ca
William-Ballesteros-Cortes <65502699+William-Ballesteros-Cortes@users.noreply.github.com>
2022-10-13 13:23:10 -0500

    Se crea el constructor del vector RN
    TODO: Crear el getter de la longitud del vector

---
+++ miVector.js
@@ -0,0 +1,17 @@
+class vectorRn {
+    constructor(values){
+        this.values = values
+    }
+
+    getLength(values){
+        for(let x = 0; )
+    }
+
+    multiplicacionEscalar(int, vectorRn){
+        for(let i=0; i < vectorRn.getLength(); i++){
+
+        }
+    }
+}
+
+testVector = new vectorRn([1,2,3])
